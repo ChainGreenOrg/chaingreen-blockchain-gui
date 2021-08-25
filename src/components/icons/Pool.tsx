aaba72e673592881fbf0f49903b3a10bcba948d7
Todor <todor@gostartups.net>
2021-08-25 12:27:45 +0300

    changes to Pool component.

--- src/components/icons/Pool.tsx
+++ src/components/icons/Pool.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@material-ui/core';
-import { ReactComponent as FarmIcon } from './images/pool.svg';
+import { ReactComponent as PoolIcon } from './images/pool.svg';
 
-export default function Farm(props: SvgIconProps) {
-  return <SvgIcon component={FarmIcon} viewBox="0 0 34 34" {...props} />;
+export default function Pool(props: SvgIconProps) {
+  return <SvgIcon component={PoolIcon} viewBox="0 0 34 34" {...props} />;
 }
